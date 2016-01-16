2d61ad0d327a4d40732272d478d1465a7e8b3e58
Adelowo Lanre <me@adelowolanre.com>
2016-01-16 06:13:13 +0100

    made method declarations in the test conform to the changes made in the source code

--- tests/FileUpload/FileNameGenerator/MD5Test.php
+++ tests/FileUpload/FileNameGenerator/MD5Test.php
@@ -2,23 +2,37 @@
 
 namespace FileUpload\FileNameGenerator;
 
+use FileUpload\FileSystem\Mock;
 use FileUpload\FileSystem\Simple;
+use FileUpload\FileUpload;
+use FileUpload\PathResolver\Simple as Path;
 
-class MD5Test extends \PHPUnit_Framework_TestCase {
+class MD5Test extends \PHPUnit_Framework_TestCase
+{
     protected $filesystem;
 
-    public function setUp() {
+    public function setUp()
+    {
 
     }
 
-    public function testGenerator() {
+    public function testGenerator()
+    {
 
         $generator = new MD5();
+        $playground_path = __DIR__ . '/../playground';
 
         $filename = "picture.jpg";
         $new_filename = md5("picture").".jpg";
 
-        $this->assertEquals($generator->getFileName($filename, "image/jpg", "asdf.jpg", 0, "100", new \FileUpload\PathResolver\Simple(''), new Simple()), $new_filename);
+        $server = array('CONTENT_TYPE' => 'image/jpg', 'CONTENT_LENGTH' => 30321);
+        $file   = array('tmp_name' => $playground_path . '/real-image.jpg', 'name' => 'real-image.jpg', 'size' => 30321, 'type' => 'image/jpg', 'error' => 0);
+
+        $fileUpload = new FileUpload($file, $server, $generator);
+        $fileUpload->setFileSystem(new Mock());
+        $fileUpload->setPathResolver(new Path($playground_path."/uploaded"));
+
+        $this->assertEquals($generator->getFileName($filename, "image/jpg", "asdf.jpg", 0, "100",$fileUpload), $new_filename);
 
     }
 
--- tests/FileUpload/FileNameGenerator/RandomTest.php
+++ tests/FileUpload/FileNameGenerator/RandomTest.php
@@ -4,15 +4,19 @@ namespace FileUpload\FileNameGenerator;
 
 use FileUpload\FileSystem\Mock;
 use FileUpload\PathResolver\Simple;
+use FileUpload\FileUpload;
 
-class RandomTest extends \PHPUnit_Framework_TestCase {
+class RandomTest extends \PHPUnit_Framework_TestCase
+{
     protected $filesystem;
 
-    public function setUp() {
+    public function setUp()
+    {
 
     }
 
-    public function testGenerator() {
+    public function testGenerator()
+    {
 
         $generator = new Random(32);
 
@@ -22,7 +26,14 @@ class RandomTest extends \PHPUnit_Framework_TestCase {
         $filesystem = new Mock();
         $resolver   = new Simple($playground_path . '/uploaded');
 
-        $new_name = $generator->getFileName($filename, "image/jpg", "asdf.jpg", 0, "100", $resolver, $filesystem);
+        $server = array('CONTENT_TYPE' => 'image/jpg', 'CONTENT_LENGTH' => 30321);
+        $file   = array('tmp_name' => $playground_path . '/real-image.jpg', 'name' => 'real-image.jpg', 'size' => 30321, 'type' => 'image/jpg', 'error' => 0);
+
+        $fileUpload = new FileUpload($file , $server , $generator);
+        $fileUpload->setPathResolver($resolver);
+        $fileUpload->setFileSystem($filesystem);
+
+        $new_name = $generator->getFileName($filename, "image/jpg", "asdf.jpg", 0, "100", $fileUpload);
 
         $this->assertEquals(32, strrpos($new_name, "."));
         $this->assertEquals(substr($new_name, strrpos($new_name, ".")), ".jpg");
