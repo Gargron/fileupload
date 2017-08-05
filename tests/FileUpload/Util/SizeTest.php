f8e842600f95b4ac38bc1b857b33d21694ce1e37
Lanre Adelowo <me@lanreadelowo.com>
2017-08-05 02:59:39 +0100

    [v2] Updated testsuite

--- tests/FileUpload/Util/SizeTest.php
+++ tests/FileUpload/Util/SizeTest.php
@@ -29,4 +29,26 @@ class SizeTest extends TestCase
             ["20GBB"]
         ];
     }
+
+    /**
+     * @dataProvider getValidInputs
+     */
+    public function testItSuccessfullyParsesAnHumanReadableString(string $input, int $expected)
+    {
+        $this->assertEquals(Size::fromHumanReadable($input), $expected);
+    }
+
+    public function getValidInputs(): array
+    {
+        return [
+            ["1B", 1],
+            ["10B", 10],
+            ["1KB", 1024],
+            ["15KB", 15360],
+            ["1MB", 1048576],
+            ["15MB",15728640],
+            ["1GB", 1073741824],
+            ["2GB", 2147483648]
+        ];
+    }
 }
