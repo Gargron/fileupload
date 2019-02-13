5524e0c9ee5846828c4e9250fd54fba6103bde86
Kruglik Denis <denis.kruglik@cortlex.com>
2019-02-13 18:39:59 +0300

    added ability to set FileNameGenerator from Factory

--- src/FileUpload/FileUploadFactory.php
+++ src/FileUpload/FileUploadFactory.php
@@ -2,6 +2,7 @@
 
 namespace FileUpload;
 
+use FileUpload\FileNameGenerator;
 use FileUpload\FileSystem\FileSystem;
 use FileUpload\PathResolver\PathResolver;
 use FileUpload\Validator\Validator;
@@ -10,7 +11,7 @@ class FileUploadFactory
 {
     /**
      * Validator to be used in the factory
-     * @var array
+     * @var Validator[]
      */
     protected $validators;
 
@@ -27,6 +28,12 @@ class FileUploadFactory
     protected $filesystem;
 
     /**
+     * FileNameGenerator to be used in the factory
+     * @var FileNameGenerator\FileNameGenerator
+     */
+    protected $fileNameGenerator;
+
+    /**
      * Construct new factory with the given modules
      * @param PathResolver $pathresolver
      * @param FileSystem   $filesystem
@@ -40,6 +47,7 @@ class FileUploadFactory
         $this->pathresolver = $pathresolver;
         $this->filesystem = $filesystem;
         $this->validators = $validators;
+        $this->fileNameGenerator = new FileNameGenerator\Simple();
     }
 
     /**
@@ -53,11 +61,22 @@ class FileUploadFactory
         $fileupload = new FileUpload($upload, $server);
         $fileupload->setPathResolver($this->pathresolver);
         $fileupload->setFileSystem($this->filesystem);
+        $fileupload->setFileNameGenerator($this->fileNameGenerator);
 
         foreach ($this->validators as $validator) {
             $fileupload->addValidator($validator);
         }
 
         return $fileupload;
+    }
+
+    /**
+     * FileNameGenerator will be used in the factory
+     *
+     * @param FileNameGenerator\FileNameGenerator $generator
+     */
+    public function withFileNameGenerator(FileNameGenerator\FileNameGenerator $generator)
+    {
+        $this->fileNameGenerator = $generator;
     }
 }
